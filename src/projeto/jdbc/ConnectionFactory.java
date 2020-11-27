a2be3e9408c0eea4cf2ed9f0c0b32f310c904ed7
Alvaro Lima <alvarolima1103@hotmail.com>
2020-11-26 21:38:04 -0400

    Criando uma conexao com banco de dados

---
+++ src/projeto/jdbc/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package projeto.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    
+    public Connection getConnection() {
+        
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/BDVENDAS", "usuarioPadrao", "123") ;
+        } catch (Exception erro) {
+            throw new RuntimeException(erro);
+        }
+        
+    }
+    
+    //connect = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=MSQLpass21!");
+    
+}
---
+++ src/projeto/jdbc/TestaConexao.java
@@ -0,0 +1,18 @@
+package projeto.jdbc;
+
+import javax.swing.JOptionPane;
+
+public class TestaConexao {
+    
+    public static void main(String[] args) {
+        
+        try {
+            new ConnectionFactory().getConnection();
+            JOptionPane.showMessageDialog(null, "Connectado com sucesso!");
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Ops aconteceu o erro: " + erro);
+        }
+        
+    }
+    
+}

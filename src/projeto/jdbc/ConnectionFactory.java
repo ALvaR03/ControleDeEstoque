23cdebbf537b5d5dfe205c057efbce3db7faf252
Alvaro Lima <alvarolima1103@outlook.com>
2021-01-30 11:26:27 -0400

    Arquivo responsavel pela conecxao com o banco de dados

--- src/projeto/jdbc/ConnectionFactory.java
+++ src/projeto/jdbc/ConnectionFactory.java
@@ -8,11 +8,10 @@ public class ConnectionFactory {
     public Connection getConnection() {
         
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/BDVENDAS", "usuarioPadrao", "123") ;
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/estoVendasJava", "usuarioVendasEsto", "123") ;
         } catch (Exception erro) {
             throw new RuntimeException(erro);
         }
-        
     }
     
     //connect = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=MSQLpass21!");

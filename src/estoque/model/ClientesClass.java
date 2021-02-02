9ca15340294bb245c118f553d450ff1500b7460a
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-01 21:51:11 -0400

    Otimizando linhas

--- src/estoque/model/ClientesClass.java
+++ src/estoque/model/ClientesClass.java
@@ -3,20 +3,9 @@ package estoque.model;
 public class ClientesClass {
 
     //Atributos
-    private int id;
-    private String nome;
-    private String rg;
-    private String cpf;
-    private String email;
-    private String telefone;
-    private String celular;
-    private String cep;
-    private String endereco;
-    private int numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String uf;
+    private int id, numero;
+    private String nome, rg, cpf, email, telefone, celular, cep;
+    private String endereco, complemento, bairro, cidade, uf;
     
     //Getters e Setters
     public int getId() {

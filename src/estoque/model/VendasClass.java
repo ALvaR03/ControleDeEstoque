926404cfc678cb4f606e8a0d0b6944901f9ada9f
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-13 16:15:21 -0400

    Incluido os atributos e metodos de acesso

--- src/estoque/model/VendasClass.java
+++ src/estoque/model/VendasClass.java
@@ -1,15 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package estoque.model;
-
 /**
  *
  * @author lima
  */
 public class VendasClass {
+    
+    // Atibutos
+    private int id;
+    private ClientesClass cliente;
+    private String data_venda;
+    private double total_venda;
+    private String obs;
+    
+    // Getter e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ClientesClass getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ClientesClass cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getData_venda() {
+        return data_venda;
+    }
+
+    public void setData_venda(String data_venda) {
+        this.data_venda = data_venda;
+    }
+
+    public double getTotal_venda() {
+        return total_venda;
+    }
+
+    public void setTotal_venda(double total_venda) {
+        this.total_venda = total_venda;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+    
     
 }

cc86e438e3f6423361b3fc828aba83cf4a8f918f
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-13 17:50:40 -0400

    Adicionado o metodo de cadastrar vendas

--- src/estoque/controller/vendas.java
+++ src/estoque/controller/vendas.java
@@ -1,6 +1,9 @@
 package estoque.controller;
 
+import estoque.model.VendasClass;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
 import projeto.jdbc.ConnectionFactory;
 
 /**
@@ -15,6 +18,28 @@ public class vendas {
          this.connect = new ConnectionFactory().getConnection();
     }
     
+    // Cadastrar Venda
+    public void cadastrarVendas(VendasClass obj) {
+        try {
+            
+            String sql = "insert into estoVendasJava.tb_produtos (cliente_id, data_venda, total_venda, observacoes) "
+                    + "values (?,?,?,?)";
+            
+            PreparedStatement stmt = connect.prepareStatement(sql);
+            stmt.setInt(1, obj.getCliente().getId());
+            stmt.setString(2, obj.getData_venda());
+            stmt.setDouble(3, obj.getTotal_venda());
+            stmt.setString(4, obj.getObs());
+            
+            stmt.execute();
+            stmt.close();
+            
+        } catch (Exception erro ) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro);
+        }
+    }
+    
+    // Retorna a ultima venda
     
     
     

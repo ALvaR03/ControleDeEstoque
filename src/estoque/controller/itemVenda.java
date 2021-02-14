9c07a6e54b0ab2bbeb5010cd7fc27ba9fe8f8da3
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-14 15:09:13 -0400

    adicionado o metodo cadastraItem

--- src/estoque/controller/itemVenda.java
+++ src/estoque/controller/itemVenda.java
@@ -2,6 +2,9 @@ package estoque.controller;
 
 import java.sql.Connection;
 import projeto.jdbc.ConnectionFactory;
+import estoque.model.ItemVendasClass;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,6 +17,30 @@ public class itemVenda {
     
     public itemVenda() {
         this.connect = new ConnectionFactory().getConnection();
+    }
+    
+    // Metodo que cadastra Itens
+    public void cadastraItem(ItemVendasClass obj) {
+        try {
+            
+            String sql = "insert into tb_itensvendas (venda_id, produto_id, qtd, subtotal) "
+                    + "values(?,?,?,?)";
+            // Peparar para conectar o banco e organizar o comando sql
+            PreparedStatement stmt = connect.prepareStatement(sql);
+            
+            stmt.setInt(1, obj.getVenda().getId());
+            stmt.setInt(2, obj.getProduto().getId());
+            stmt.setDouble(3, obj.getQtd());
+            stmt.setDouble(4, obj.getSubtotal());
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Item Registrado com sucesso"); 
+            
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Erro: " + erro);
+        }
     }
     
 }

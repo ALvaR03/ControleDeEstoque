5d47c15d4454f39d60689fabe7e97fc3f6f17424
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-14 16:41:32 -0400

    linha adicionada para receber os itens do carrinho de compras

--- src/estoque/view/FrmVendas.java
+++ src/estoque/view/FrmVendas.java
@@ -400,7 +400,10 @@ public class FrmVendas extends javax.swing.JFrame {
         //Botao Pagamento
         FrmPagamentos telap = new FrmPagamentos();
         telap.txtTotal.setText(String.valueOf(total));
-        telap.cliente_id = obj.getId(); // passando o que tem dentro do obj para o id do cliente
+        
+        telap.clienteObj = obj; // passando um objeto inteiro para outra tela
+        telap.carrinho = carrinho;
+        
         telap.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_btnPagamentoActionPerformed

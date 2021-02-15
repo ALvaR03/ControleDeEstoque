c1a3f540e5436295369e218b0b8ba2477069e1d4
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-15 19:15:22 -0400

    habilitando o dispose

--- src/estoque/view/FrmCliente.java
+++ src/estoque/view/FrmCliente.java
@@ -99,7 +99,8 @@ public class FrmCliente extends javax.swing.JFrame {
         btnEditar = new javax.swing.JButton();
         btnExcluir = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Cliente");
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowActivated(java.awt.event.WindowEvent evt) {
                 formWindowActivated(evt);
